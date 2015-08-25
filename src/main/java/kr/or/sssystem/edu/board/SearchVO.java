e78a84cc742a6e748cbae7dc046144695e2329ed
Hyunsu <Hyunsu@Hyunsu-PC>
2015-08-25 17:12:15 +0900

    ㅎㅎㅎㅎ 오류재생기

---
+++ src/main/java/kr/or/sssystem/edu/board/SearchVO.java
@@ -0,0 +1,62 @@
+package kr.or.sssystem.edu.board;
+
+public class SearchVO {
+	
+	private String searchTp;
+	private String searchWord;
+	private int board_gb;
+	private int page_no;
+	private int total;
+
+	public SearchVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getSearchTp() {
+		return searchTp;
+	}
+
+	public void setSearchTp(String searchTp) {
+		this.searchTp = searchTp;
+	}
+
+	public String getSearchWord() {
+		return searchWord;
+	}
+
+	public void setSearchWord(String searchWord) {
+		this.searchWord = searchWord;
+	}
+
+	public int getBoard_gb() {
+		return board_gb;
+	}
+	
+	public void setBoard_gb(int board_gb) {
+		this.board_gb = board_gb;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchBean [board_gb=" + board_gb + ", searchTp=" + searchTp + ", searchWord="
+				+ searchWord + "]";
+	}
+
+	public int getPage_no() {
+		return page_no;
+	}
+
+	public void setPage_no(int page_no) {
+		this.page_no = page_no;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	
+}
