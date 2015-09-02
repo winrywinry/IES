d61075edb0dc391d514fd750421f278740530f76
birdmissile <birdmissile>
2015-09-02 21:49:20 +0900

    123

--- src/main/java/com/sssystem/edu/service/TestService.java
+++ src/main/java/com/sssystem/edu/service/TestService.java
@@ -1,5 +1,6 @@
 package com.sssystem.edu.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.sssystem.edu.vo.TestVO;
@@ -11,4 +12,6 @@ public interface TestService {
 	public boolean update(TestVO testVO);
 	public boolean delete(int test_no);
 	public List<TestVO> selectTest(int edu_no);
+	public int countTest (int edu_no);
+	public TestVO selectAnswer(int edu_no,int q_no);
 }
